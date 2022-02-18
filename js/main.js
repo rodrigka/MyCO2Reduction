ca2cad6a6c10852979939cf6e1445f0d90281061
rodrigka <91433354+rodrigka@users.noreply.github.com>
2022-02-18 17:16:20 +0100

    Avancement 1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //exécution fonctions
-document.getElementById("btn__se-connecter").addEventListener("click", seconnecter);
-document.getElementById("btn__s-enregistrer").addEventListener("click", register);
+document.getElementById("btn__session").addEventListener("click", seconnecter);
+document.getElementById("btn__register").addEventListener("click", register);
 window.addEventListener("resize", anchoPage);
 
 //Variables
@@ -11,54 +11,7 @@ var box_back_login = document.querySelector(".box__back-login");
 var box_back_register = document.querySelector(".box__back-register");
 
     //FONCTIONS
-
-function anchoPage(){
-
-    if (window.innerWidth > 850){
-        box_back_register.stylesep.display = "block";
-        box_back_login.stylesep.display = "block";
-    }else{
-        box_back_register.stylesep.display = "block";
-        box_back_register.stylesep.opacity = "1";
-        box_back_login.stylesep.display = "none";
-        form_login.stylesep.display = "block";
-        container_login_register.stylesep.left = "0px";
-        form_register.stylesep.display = "none";   
-    }
-}
-
-anchoPage();
-
-
-    function seconnecter(){
-        if (window.innerWidth > 850){
-            form_login.stylesep.display = "block";
-            container_login_register.stylesep.left = "10px";
-            form_register.stylesep.display = "none";
-            box_back_register.stylesep.opacity = "1";
-            box_back_login.stylesep.opacity = "0";
-        }else{
-            form_login.stylesep.display = "block";
-            container_login_register.stylesep.left = "0px";
-            form_register.stylesep.display = "none";
-            box_back_register.stylesep.display = "block";
-            box_back_login.stylesep.display = "none";
-        }
-    }
-
-    function register(){
-        if (window.innerWidth > 850){
-            form_register.stylesep.display = "block";
-            container_login_register.stylesep.left = "410px";
-            form_login.stylesep.display = "none";
-            box_back_register.stylesep.opacity = "0";
-            box_back_login.stylesep.opacity = "1";
-        }else{
-            form_register.stylesep.display = "block";
-            container_login_register.stylesep.left = "0px";
-            form_login.stylesep.display = "none";
-            box_back_register.stylesep.display = "none";
-            box_back_login.stylesep.display = "block";
-            box_back_login.stylesep.opacity = "1";
-        }
+function affichage() {
+    var div = document.getElementById("registerDIV");
+    if (div.classList.contains("cacher") == true){div.classList.toggle('afficher');}
 }
